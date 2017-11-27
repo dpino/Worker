1c64c64d0eeac4c9d42299c75969d2053367273a
Diego Pino Garcia <dpino@igalia.com>
2017-11-27 09:40:36 +0100

    Run nan-timeout test in a browser

---
+++ browser-harness.js
@@ -0,0 +1,40 @@
+Worker.create = function(source_text) {
+    let blob = new Blob([source_text], {type: 'text/javascript'});
+    let url = URL.createObjectURL(blob);
+    return new Worker(url);
+}
+
+function enterEventLoop() {}
+
+function exitEventLoop(w) {
+    if (w.constructor === Array) {
+        w.forEach((w) => w.terminate());
+    } else if (typeof w === 'object') {
+        w.terminate();
+    }
+}
+
+function sleep(ns) {
+    function wait(sec) {
+        let start = Date.now();
+        while (true) {
+            if (Date.now() - start > sec) {
+                return;
+            }
+        }
+    }
+    wait(ns);
+    return {
+        then: function(fn) {
+            if (fn) { fn(); }
+        }
+    }
+}
+
+assert = {
+    'sameValue': function(a, b) {
+        if (a !== b) {
+            throw new Error('not equals: ' + a + " != " + b);
+        }
+    }
+}
